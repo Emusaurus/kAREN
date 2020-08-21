ec436852a6ca6a8c6ea92197c4688f1ecf6b5377
Andrew <realglitch337@gmail.com>
2020-08-21 14:14:26 -0400

    New JS, seperated from /templates/index.html

---
+++ static/js/rogue.js
@@ -0,0 +1,18 @@
+function reportSuccess() {
+    window.alert("Successfully reported rogue karen")
+}
+
+function reportRogueKaren() {
+    reports = 1
+    if (reports = 1) {
+
+        $.ajax({
+            type: "POST",
+            url: https: //maker.ifttt.com/trigger/karen_rogue_reported/with/key/cpUmNJz0j4Hn-qeRou7DlI,
+            data: data,
+            success: reportSuccess(),
+            dataType: text
+        });
+
+    } else window.alert("You've already reported rogue karen.")
+}
