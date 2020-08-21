80f34b455a35a706a7495f840846ef20923ce186
Andrew <realglitch337@gmail.com>
2020-08-21 14:42:28 -0400

    Tried to remove template caching
    Fuck template caching, all my homies hate template caching.

--- app.py
+++ app.py
@@ -4,11 +4,19 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 import sys
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
 trainer = ChatterBotCorpusTrainer(english_bot)
 #trainer.train("chatterbot.corpus.english")
-#trainer.train("chatterbot.corpus.cut_from_english")
+#trainer.train("chatterbot.corpus.tard")
+
+@app.before_request
+def before_request():
+    # When you import jinja2 macros, they get cached which is annoying for local
+    # development, so wipe the cache every request.
+    if 'localhost' in request.host_url or '0.0.0.0' in request.host_url:
+        app.jinja_env.cache = {}
 
 @app.route("/")
 def home():
@@ -21,4 +29,4 @@ def get_bot_response():
     return response
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, threaded=True)
+    app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
